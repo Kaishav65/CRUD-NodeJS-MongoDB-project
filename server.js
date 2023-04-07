ec903d903fc0dac2a83637f5ab35c3b24e48054c
kaishav basodiya <kaishavbasodiya@gmail.com>
2023-04-07 12:05:37 +0530

    new env api

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
-
+dotenv.config()
 const EmployeeRoute = require('./routes/EmployeeRoutes')
 // mongoose.connect('mongodb://localhost:27017/testdb',{useNewUrlParser:true,useUnifiedTopology:true})
-mongoose.connect('mongodb+srv://kaishav:rootpassword@clusterkeshav.6qezoi5.mongodb.net/testdb?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+
+// mongoose.connect('mongodb+srv://kaishav:rootpassword@clusterkeshav.6qezoi5.mongodb.net/testdb?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.MDB,{useNewUrlParser:true,useUnifiedTopology:true})
 const db = mongoose.connection;
 
 db.on('error', err =>{
